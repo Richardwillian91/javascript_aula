ba3787a2876fb6b5e8700c1ce7a613d6d9ec1f15
Richard Silva <richardwillian166@hotmail.com>
2020-12-29 18:06:19 -0300

    Exercicios javascript

---
+++ js/main.js
@@ -0,0 +1,103 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML=" <b> Obrigado por clicar </b>";
+}
+
+/*var validar = 0;
+
+
+function validaIdade(idade){
+    if (idade>=18){
+        validar =  true;
+    }
+    else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+validaIdade(idade);
+console.log(validar);*/
+
+/*function soma(n1,n2){
+    return n1+n2;
+}
+alert(soma(5,10));
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+
+
+/*var d = new Date();
+alert(d);
+alert(d.getMinutes());
+alert(d.getMonth());
+
+/*var count;
+for(count=0; count <=5; count++){
+alert(count);
+};
+
+/*var count = 0;
+while(count <=5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+
+
+
+/*var idade= prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+
+
+/*var frutas = [{nome:"maçã", cor:"vermelho"}, {nome:"uva",cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+
+var lista = ["maça", "pêra", "laranja"];
+console.log(lista);
+lista.push("uva");
+console.log(lista)
+lista.pop();
+console.log(lista.length);
+
+
+
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.toString()[0])
+console.log(lista.join(" -"));
+console.log(lista.join(" |"));
+
+
+var nome = "Richard Willian";
+var idade = 29;
+var idade2 = 10
+var frase = "Japão é a melhor seleção do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert (idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+console.log (frase.toUpperCase());
+console.log (frase.toLowerCase());
+
+var n1 = 5;
+var n2 = 7;
+
+console.log(n1 * n2);
+console.log(n1 + n2);
+console.log(n1 / n2);
+console.log(n1 - n2);*/
